b727ef01d68a96a85236b5ab9408633d69d15408
BDCarrillo <90526940+BDCarrillo@users.noreply.github.com>
2022-02-01 21:27:23 -0600

    Added Vars

--- Data/Scripts/CoreParts/script/Structure.cs
+++ Data/Scripts/CoreParts/script/Structure.cs
@@ -438,6 +438,9 @@ namespace Scripts
                     [ProtoMember(18)] internal bool SpinFree;
                     [ProtoMember(19)] internal bool StayCharged;
                     [ProtoMember(20)] internal int MagsToLoad;
+                    [ProtoMember(21)] internal int MaxActiveProjectiles;
+                    [ProtoMember(22)] internal int MaxReloads;
+
                 }
 
 
