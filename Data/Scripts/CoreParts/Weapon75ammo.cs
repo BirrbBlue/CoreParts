9d0876a4eee385fd98a16670912d0640a96e7382
BDCarrillo <90526940+BDCarrillo@users.noreply.github.com>
2022-06-19 20:43:21 -0500

    Comment updates

--- Data/Scripts/CoreParts/Weapon75ammo.cs
+++ Data/Scripts/CoreParts/Weapon75ammo.cs
@@ -281,7 +281,7 @@ namespace Scripts
                     TrackingDelay = 0, // Measured in Shape diameter units traveled.
                     MaxChaseTime = 0, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     OverideTarget = true, // when set to true ammo picks its own target, does not use hardpoint's.
-                    CheckFutureIntersection = false, // Utilize obstacle avoidance?
+                    CheckFutureIntersection = false, // Utilize obstacle avoidance for drones
                     MaxTargets = 0, // Number of targets allowed before ending, 0 = unlimited
                     NoTargetExpire = false, // Expire without ever having a target at TargetLossTime
                     Roam = false, // Roam current area after target loss
@@ -646,7 +646,7 @@ namespace Scripts
                     TrackingDelay = 5, // Measured in Shape diameter units traveled.
                     MaxChaseTime = 450, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     OverideTarget = true, // when set to true ammo picks its own target, does not use hardpoint's.
-                    CheckFutureIntersection = false, // Utilize obstacle avoidance?
+                    CheckFutureIntersection = false, // Utilize obstacle avoidance for drones
                     MaxTargets = 3, // Number of targets allowed before ending, 0 = unlimited
                     NoTargetExpire = false, // Expire without ever having a target at TargetLossTime
                     Roam = false, // Roam current area after target loss
