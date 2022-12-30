a9744c627237a286e14f11b5369c6c3c32cd2485
Shane Stixrud <shane@stixrud.org>
2022-12-29 19:49:45 -0800

    ArmWhenShot

--- Data/Scripts/CoreParts/script/Structure.cs
+++ Data/Scripts/CoreParts/script/Structure.cs
@@ -926,8 +926,7 @@ namespace Scripts
                     [ProtoMember(11)] internal TimedSpawnDef TimedSpawns;
                     [ProtoMember(12)] internal bool FireSound; // not used, can remove
                     [ProtoMember(13)] internal Vector3D AdvOffset;
-
-                    [ProtoContract]
+                    [ProtoMember(14)] internal bool ArmWhenShot;
                     public struct TimedSpawnDef
                     {
                         public enum PointTypes
